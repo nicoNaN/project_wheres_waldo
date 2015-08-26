a47fbfb05326a8653179e5097797c8592807e957
niconan <nick.hax@gmail.com>
2015-08-25 21:08:16 -0700

    box around middle of click

--- app/assets/javascripts/waldo.js
+++ app/assets/javascripts/waldo.js
@@ -7,12 +7,23 @@ var waldoCalls = {
 
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
-    ctx.rect(relativeX, relativeY,150,100);
+    ctx.rect(relativeX - 25, relativeY - 25,50,50);
     ctx.stroke();
+  },
+
+  drawWaldo: function() {
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+    var img = new Image();
+    img.onload = function() {
+      ctx.drawImage(img, 0, 0);
+    };
+    img.src = "https://cldup.com/bR93jdzyuH.jpg";
   }
 };
 
 $(document).on('page:change', function() {
+  waldoCalls.drawWaldo();
   $('#waldo').click(function(e) {
     waldoCalls.drawMouseBox(this, e);
   });
